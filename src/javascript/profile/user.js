5e9948d98e31756fa0068fc58b8d162d8a05b56b
pnordboj <91888307+pnordboj@users.noreply.github.com>
2022-12-15 11:38:21 +0100

    Fixed CSS and added some new functions

--- src/javascript/profile/user.js
+++ src/javascript/profile/user.js
@@ -12,33 +12,54 @@ const headers = {
 export async function getUser() {
     const response = await fetch(url, {
         headers: headers,
+        'content-type': 'application/json',
     });
     const data = await response.json();
     console.log(data);
-    const user = document.getElementById('user-container');
-    user.innerHTML = '';
-    user.innerHTML = `
-        <div id="user-box" class="card-body">
-            <div class="card" id="card-image"> 
-                <img id="user-image" src="${data.avatar}" class="card-img-top" alt="${data.name} (No profile picture)">
-                <button id="edit-image-btn" class="btn btn-secondary">Edit Image</button>
-            </div>
-            <div id="user-info">
-                <label id="username-label">Username:</label>
-                <div id="user-title-box">
-                    <p class="card-title
-                    ">${data.name}</p>
-                    <button id="edit-name-btn" class="btn btn-secondary">Edit Username</button>
-                </div>
-                <hr id="whiteline">
-                <label id="user-email-label">Email:</label>
-                <div id="user-email-box">
-                    <p class="card-text">${data.email}</p>
-                    <button id="edit-email-btn" class="btn btn-secondary">Edit</button>
-                </div>
-                <hr id="whiteline">
-                <p class="card-text">Credits available: ${data.credits}</p>
-            </div>
-        </div>
+    const cardImage = document.getElementById('card-image');
+    const userName = document.getElementById('user-title-box');
+    const userEmail = document.getElementById('user-email-box');
+    const totalCredits = document.getElementById('total-credits');
+    cardImage.innerHTML = `
+        <img id="user-image" src="${data.avatar}" class="card-img-top" alt="${data.name} (No profile picture)">
     `;
+    userName.innerHTML = `
+        <p class="card-title">${data.name}</p>
+    `;
+    userEmail.innerHTML = `
+        <p class="card-text">${data.email}</p>
+    `;
+    totalCredits.innerHTML = `
+        <p class="card-text">Credits available: ${data.credits}</p>
+    `;
+}
+
+export async function editImage() {
+    const newImg = document.getElementById('new-image-url').value;
+    const body = JSON.stringify({
+        avatar: newImg,
+    });
+    const response = await fetch(url, {
+        method: 'PUT',
+        headers: headers,
+        body: body,
+    });
+    const data = await response.json();
+    console.log(data);
+    if (response.status === 200) {
+        alert.innerHTML = `
+            <div class="alert alert-success" role="alert">
+                Image updated!
+            </div>
+        `;
+        setTimeout(() => {
+            location.reload();
+        }, 1000);
+    } else {
+        alert.innerHTML = `
+            <div class="alert alert-danger" role="alert">
+                Something went wrong!
+            </div>
+        `;
+    }
 }
